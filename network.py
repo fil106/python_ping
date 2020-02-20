248ad15066f0d34ac22bad1f005f05fe8fa32eac
Philipp Fyodorov <fil106@mail.ru>
2020-02-20 23:31:35 +0300

    Add files via upload
    First commit. Ping hosts by input subnet

---
+++ network.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from pythonping import ping
+import ipaddress
+
+#Получаем подсеть от пользователя
+user_subnet = ipaddress.ip_network(input('Какую сеть пингуем?:'))
+#Фиксируем время начала выполнения скрипта
+time_start = datetime.now()
+
+print ('---Запускаю пинги...')
+for ip in user_subnet:
+
+	if ip == user_subnet[0]:
+		print (str(ip) + ' - шлюз')
+		continue
+	elif ip == user_subnet.broadcast_address:
+		print (str(ip) + ' - широковещательный адрес')
+		continue
+	
+	# Результат пинга помещаем в response_list
+	response_list = ping(str(ip), count=2)
+
+	if response_list.success():
+		print (str(ip) + ' - доступен')
+	else:
+		print (str(ip) + ' - недоступен')
+print ('---Пинги закончены!')
+
+#Фиксируем время конца выполнения скрипта
+time_end = datetime.now()
+#Вычитаем из конца - начало и получаем результат
+time_result = time_end - time_start
+print ('Скрипт выполнился за: ', time_result)
